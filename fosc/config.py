fdd0148a7ebb3ffc7618c76060496c19dd06bdf3
Tobias Weber <mail@tgweber.de>
2020-02-25 06:55:39 +0100

    Updated faulty mlp_l model

--- fosc/config.py
+++ fosc/config.py
@@ -22,7 +22,7 @@ config = {
          "mlp_l": {
             "id": "mlp_l",
             "type": "mlp",
-            "url": "https://zenodo.org/record/3685204/files/mlp_l.tar.gz"
+            "url": "https://zenodo.org/record/3686547/files/mlp_l.tar.gz"
         },
         "lstm_s": {
             "id": "lstm_s",
